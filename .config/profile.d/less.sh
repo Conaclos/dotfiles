bc1ef2e0da8eaf0d55ac07ef50558bf90f0f1ad1
Victorien Elvinger <victorien@elvinger.fr>
2017-03-17 00:11:40 +0100

    feat(profiles): add user profiles
    - Make cargo XDG-compliant (expected to be a temporary fix)
    - Set $PAGER and $VISUAL
    - Add default arguments for less

---
+++ .config/profile.d/less.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# zlib license
+# Copyright (c) 2017 Victorien Elvinger
+#
+
+export LESS='--ignore-case -g -M -R -w --window=-5'
+    # Default Less options
+    # g: only highlight the last search (perf)
+    # M: be more verbose than more (long prompt)
+    # R: output ANSI "color" escape sequences in "raw" form
+    # w: highlights  the  first  "new"  line after a forward
+
